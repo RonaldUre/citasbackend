2be1f1b6f3ff2b0300065f4f35c4b852e34d08ef
ronald <roneduardo2014@gmail.com>
2025-08-05 15:35:03 -0400

    cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:5173', // o '*', pero más seguro definir el origen
+    origin: ['http://localhost:5173', 'https://citas-m8uo507kc-ronald-ures-projects.vercel.app/'], // o '*', pero más seguro definir el origen
     credentials: true, // si usas cookies
   });
   
